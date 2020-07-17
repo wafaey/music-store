0c70be81595c817796f94f7377cddedc56d0378d
wafaey <omarwafaey54@gmail.com>
2020-07-17 17:44:06 +0200

    add build

---
+++ build/precache-manifest.0fdcb659ae9099be57f6d3167bd9865a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af09b894e8db895cbf6636214a24d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fd659b7dd12e984e619",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2ee6ec8c17031120b873",
+    "url": "/static/css/main.ee05588d.chunk.css"
+  },
+  {
+    "revision": "8fd659b7dd12e984e619",
+    "url": "/static/js/2.f08968ca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f08968ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee6ec8c17031120b873",
+    "url": "/static/js/main.b1090bd7.chunk.js"
+  },
+  {
+    "revision": "47d251e64c171289d82d",
+    "url": "/static/js/runtime-main.a82171c2.js"
+  }
+]);
